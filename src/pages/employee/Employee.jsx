163412bb3ccf87996a98fed6d584569cbac99fa8
T0nyBlue <18521123@gm.uit.edu.vn>
2021-12-06 20:52:23 +0700

    update

--- src/pages/employee/Employee.jsx
+++ src/pages/employee/Employee.jsx
@@ -89,11 +89,7 @@ export default function Employee() {
       <div className="userContainer">
         <div className="userShow">
           <div className="userShowTop">
-            <img
-              src="https://th.bing.com/th/id/R.671ad41c4dee19bf2634551bd6b10297?rik=THGL4iCpBjKMpQ&riu=http%3a%2f%2fwww.nzasianleaders.com%2fwp-content%2fuploads%2fdefault-avatar.jpg&ehk=lmPQ78hkERVbp7zfg5vN7g1qf3jMZ%2feDS5UeyJ%2bk4Cw%3d&risl=&pid=ImgRaw&r=0"
-              alt=""
-              className="userShowImg"
-            />
+            <img src={data.Img} alt="" className="userShowImg" />
             <div className="userShowTopTitle">
               <span className="userShowUserName">{data.Name}</span>
               <span className="userShowUserTitle">{data.Type}</span>
